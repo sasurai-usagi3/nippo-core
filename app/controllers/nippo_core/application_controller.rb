c0a68927bf32796c8f55c72de0007f0f62bdb138
宇山拓夢 <sasurai.usagi3@gmail.com>
2017-04-22 15:16:59 +0900

    authenticate_user!をまとめた

--- app/controllers/nippo_core/application_controller.rb
+++ app/controllers/nippo_core/application_controller.rb
@@ -1,6 +1,8 @@
 module NippoCore
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :authenticate_user!
+
   private
     def find_group
       @group = NippoCore::Group.find(params[:group_id])
